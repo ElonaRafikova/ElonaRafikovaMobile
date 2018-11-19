97385a8e8ecf356b47328bfa1111f3f846112da4
el.raf <36075706+elonarafikova@users.noreply.github.com>
2018-11-19 18:20:38 +0300

    hw3 new properties files enums

--- src/main/java/enums/PropertiesFile.java
+++ src/main/java/enums/PropertiesFile.java
@@ -3,13 +3,23 @@ package enums;
 public enum PropertiesFile {
     WEB("webtests.properties"),
     NATIVE("nativetests.properties"),
-    WEB_IPHONE(""),
-    NATIVE_ANDROID("");
+    WEB_IPHONE5S("IPhone5S_webtests.properties"),
+    NATIVE_NEXUS5("Nexus5_nativetests.properties");
 
     private String type;
 
     PropertiesFile(String type) {
         this.type = type;
+    }
+
+    public static PropertiesFile find(String type) throws Exception {
+        PropertiesFile[] values = PropertiesFile.values();
+        for (PropertiesFile value : values) {
+            if (type.equals(value.getType())) {
+                return value;
+            }
+        }
+        throw new Exception();
     }
 
     public String getType() {
